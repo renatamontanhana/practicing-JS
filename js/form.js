d62a56e55e8a70fe7fa758426a9b651c20742eb5
Renata Montanhana <112817008+renatamontanhana@users.noreply.github.com>
2023-03-09 18:37:16 -0400

    Add files via upload

--- js/form.js
+++ js/form.js
@@ -7,10 +7,9 @@ botaoAdicionar.addEventListener("click", function(event){
     var pacienteTr = montaTr(paciente);
 
     var erros = validaPaciente(paciente);
-
-    if (erros.length > 0){
-        var mensagemErro = document.querySelector("#mensagem-erro");
-        mensagemErro.textContent = erros;
+    console.log(erros);
+    if(erros.length > 0){
+        exibeMensagensDeErro(erros);
         return;
     }
 
@@ -21,8 +20,21 @@ botaoAdicionar.addEventListener("click", function(event){
     tabela.appendChild(pacienteTr);
 
     form.reset();
+    var mensagensErro = document.querySelector("mensagens-erro");
+    mensagensErro.innerHTML = "";
 
 });
+function exibeMensagensDeErro(erros) {
+    var ul = document.querySelector("#mensagens-erro");
+    ul.innerHTML = "";
+
+    erros.forEach(function(erro) {
+        var li = document.createElement("li");
+        li.textContent = erro;
+        ul.appendChild(li);
+    });
+}
+
 function obtemPacienteDoFormulario(form){
     var paciente = {
         nome: form.nome.value,
@@ -55,15 +67,31 @@ function montaTd(dado,classe){
     return td;
 }
 
-function validaPaciente(paciente){
+function validaPaciente(paciente) {
 
     var erros = [];
 
-    if (!validaPeso(paciente.peso)) {
+    if (paciente.nome.length == 0){
+        erros.push("O nome não pode ser em branco");
+    }
+
+    if (paciente.gordura.length == 0){
+        erros.push("A gordura não pode ser em branco");
+    }
+
+    if (paciente.peso.length == 0){
+        erros.push("O peso não pode ser em branco");
+    }
+
+    if (paciente.altura.length == 0){
+        erros.push("A altura não pode ser em branco");
+    }
+
+    if (!validaPeso(paciente.peso)){
         erros.push("Peso é inválido");
     }
 
-    if (!validaAltura(paciente.altura)) {
+    if (!validaAltura(paciente.altura)){
         erros.push("Altura é inválida");
     }
 
